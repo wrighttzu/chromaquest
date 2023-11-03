bf545335cb21df6d9e162d5f2ed8acf224d66880
Wright Tzu <wrighttzu@gmail.com>
2023-11-03 13:55:56 -0300

    status bar defined

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,21 +1,24 @@
 import * as React from "react";
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import Settings from "./screens/Settings";
 import About from "./screens/About";
 import GameScreen from "./screens/GameScreen";
-
+import { StatusBar } from "react-native";
 
 const AppStack = createStackNavigator();
 const AppStackNavigator = () => {
   return (
-    <AppStack.Navigator screenOptions={{ headerShown: false }}>
-      <AppStack.Screen name="Home" component={HomeScreen}/>
-      <AppStack.Screen name="Settings" component={Settings}/>
-      <AppStack.Screen name="About" component={About}/>
-      <AppStack.Screen name="GameScreen" component={GameScreen}/>
-    </AppStack.Navigator>
+    <>
+      <StatusBar backgroundColor="#FFFFFF" barStyle="dark-content" />
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Home" component={HomeScreen} />
+        <AppStack.Screen name="Settings" component={Settings} />
+        <AppStack.Screen name="About" component={About} />
+        <AppStack.Screen name="GameScreen" component={GameScreen} />
+      </AppStack.Navigator>
+    </>
   );
 };
 
